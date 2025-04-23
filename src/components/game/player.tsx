44e42de1be25a89beccec9455e5e49701ce983c0
Jeffreyyvdb <jeffrey.2001.2312@gmail.com>
2025-04-23 18:18:59 +0200

    Use the studdy buddy robot

--- src/components/game/player.tsx
+++ src/components/game/player.tsx
@@ -1,10 +1,18 @@
 import React from "react";
+import Image from "next/image";
 
 export const Player: React.FC = () => {
   return (
     <div className="absolute bottom-20 left-1/2 transform -translate-x-1/2 z-20">
-      <div className="w-16 h-16 bg-blue-500 rounded-full flex items-center justify-center shadow-lg">
-        <span className="text-white text-3xl">🚶</span>
+      <div className="w-32 h-32 flex items-center justify-center shadow-lg">
+        <Image
+          src="/images/study-buddy.png"
+          alt="Study Buddy character"
+          width={100}
+          height={100}
+          priority
+          className="object-contain"
+        />
       </div>
     </div>
   );
